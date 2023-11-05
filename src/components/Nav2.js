058548665a75ff18e601ad65c4114291d5c2031d
Amir Khan <amirkhan1985@gmail.com>
2023-11-05 23:39:12 +0530

    newdata

---
+++ src/components/Nav2.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function Nav2(){
+      return(
+        <>
+        <div className="container" style={{backgroundColor:'blue'}}>
+        <nav>
+         <ul>
+             <li><a href="">Home</a></li>
+             <li><a href="">Contact</a></li>
+             <li><a href="">Service</a></li>
+         </ul>
+        </nav>
+ 
+        </div>
+       
+        </>
+      )
+}
