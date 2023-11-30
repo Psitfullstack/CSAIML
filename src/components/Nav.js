914b7473fae00d782183500c5c12c93c60bc0363
Amir Khan <amirkhan1985@gmail.com>
2023-11-30 09:50:20 +0530

    update

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,12 +5,41 @@ function Nav(){
     const body=document.getElementById('body')
 
     const card =document.getElementsByClassName('container')
+    // const card =document .getElementsByClassName('card')
+    const btn1=document.getElementsByClassName('btn1')
     
+    console.log(btn1)
+    var flag=true
      
     function handler(){
+
+        if (flag){
           body.style.backgroundColor='black'
-          card[0].firstChild.style.backgroundColor='black'
-          card[0].firstChild.style.color='white'
+          card[0].childNodes[0].style.backgroundColor='black'
+          card[0].childNodes[0].style.color='white'
+          card[0].childNodes[1].style.backgroundColor='black'
+          card[0].childNodes[1].style.color='white'
+          card[0].childNodes[2].style.backgroundColor='black'
+          card[0].childNodes[2].style.color='white'
+          btn1[0].innerHTML='Normal Mode'
+
+        }
+
+        else{
+          body.style.backgroundColor='white'
+          card[0].childNodes[0].style.backgroundColor='white'
+          card[0].childNodes[0].style.color='black'
+          card[0].childNodes[1].style.backgroundColor='white'
+          card[0].childNodes[1].style.color='black'
+          card[0].childNodes[2].style.backgroundColor='white'
+          card[0].childNodes[2].style.color='black'
+          btn1[0].innerHTML='DarkMode'
+
+        }
+
+        flag=!flag
+         
+      
     }
 
     return(
@@ -25,7 +54,7 @@ function Nav(){
         </ul>
        </nav>
 
-     <button className='btn1' onClick={handler} >Dark Mode</button>
+     <button className='btn1' id='btn2' onClick={handler} >Dark Mode</button>
 
        </div>
 
